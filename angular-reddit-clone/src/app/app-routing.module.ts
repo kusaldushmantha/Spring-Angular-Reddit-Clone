10a560b7c6b1aa5a5b41fca5ac8eaf1f654daba1
kusaldushmantha <kusald.14@cse.mrt.ac.lk>
2020-07-28 19:18:27 +0530

    Subreddit navigation and add post added

--- angular-reddit-clone/src/app/app-routing.module.ts
+++ angular-reddit-clone/src/app/app-routing.module.ts
@@ -1,14 +1,20 @@
-import {NgModule} from '@angular/core';
-import {RouterModule, Routes} from '@angular/router';
-import {SignupComponent} from './auth/signup/signup.component';
-import {LoginComponent} from './auth/login/login.component';
-import {HomeComponent} from './home/home.component';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { SignupComponent } from './auth/signup/signup.component';
+import { LoginComponent } from './auth/login/login.component';
+import { HomeComponent } from './home/home.component';
+import { CreatePostComponent } from './post/create-post/create-post.component';
+import { CreateSubredditComponent } from './subreddit/create-subreddit/create-subreddit.component';
+import { ListSubredditsComponent } from './subreddit/list-subreddits/list-subreddits.component';
 
 
 const routes: Routes = [
-  {path: 'sign-up', component: SignupComponent},
-  {path: 'login', component: LoginComponent},
-  {path: '', component: HomeComponent},
+  { path: 'sign-up', component: SignupComponent },
+  { path: 'login', component: LoginComponent },
+  { path: '', component: HomeComponent },
+  { path: 'create-post', component: CreatePostComponent },
+  { path: 'create-subreddit', component: CreateSubredditComponent },
+  { path: 'list-subreddits', component: ListSubredditsComponent }
 ];
 
 @NgModule({
